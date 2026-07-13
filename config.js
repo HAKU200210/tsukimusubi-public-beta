7ff38c72afde72a9c6e1631e6b3ca58b92e351c1
HAKU200210 <HAKU200210@users.noreply.github.com>
2026-07-14 01:46:18 +0900

    Connect published LINE MINI App

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.TSUKIMUSUBI_CONFIG = {
   supabaseUrl: 'https://zvbvxssyrxmjdvwevows.supabase.co',
   supabaseKey: 'sb_publishable_rZSa6C7dmPPiCKaoQKcBqw_Ip_yJF3R',
-  liffId: '2010692528-SxJ3tVeC'
+  liffId: '2010692530-iFgEENZt'
 };
