63f88a19575a72b1fcfb52b8345f2bf1bdda8573
HAKU200210 <haku2002ovo@gmail.com>
2026-07-14 01:27:06 +0900

    Connect LINE MINI App

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.TSUKIMUSUBI_CONFIG = {
   supabaseUrl: 'https://zvbvxssyrxmjdvwevows.supabase.co',
   supabaseKey: 'sb_publishable_rZSa6C7dmPPiCKaoQKcBqw_Ip_yJF3R',
-  liffId: ''
+  liffId: '2010692528-SxJ3tVeC'
 };
