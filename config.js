61f0611702322dfb3119708faf0d59a2ce7007bd
HAKU200210 <haku2002ovo@gmail.com>
2026-07-14 00:42:59 +0900

    Connect public Supabase project
    * Connect public Supabase project
    
    * Add public Supabase publishable key

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.TSUKIMUSUBI_CONFIG = {
-  supabaseUrl: '',
-  supabaseKey: '',
+  supabaseUrl: 'https://zvbvxssyrxmjdvwevows.supabase.co',
+  supabaseKey: 'sb_publishable_rZSa6C7dmPPiCKaoQKcBqw_Ip_yJF3R',
   liffId: ''
 };
